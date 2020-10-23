78a446a15a9117585edb1f3ac35add195d291902
lisastriker <lisastriker@gmail.com>
2020-10-23 11:14:05 +0800

    Commit the mysql

--- config/database.php
+++ config/database.php
@@ -43,7 +43,7 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
-        'mysql' => [
+        /*'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -61,6 +61,22 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+        ],*/
+
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => 'us-cdbr-east-02.cleardb.com',
+            'port' => env('DB_PORT', '3306'),
+            'database' => 'heroku_1b1e379a4785776',
+            'username' => 'b5212857f23940',
+            'password' => 'e5b7403f',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
         ],
 
         'pgsql' => [
