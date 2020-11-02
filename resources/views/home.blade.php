fdf8d21bd2825f45f72c6149c7303d7e102a44ab
lisastriker <lisastriker@gmail.com>
2020-11-02 10:58:19 +0800

    Branch commit message

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
                 <div>Making changes in laravel</div>
+                <div>Making changes for branching</div>
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
